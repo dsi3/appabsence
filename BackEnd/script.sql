2d16eede0fc0c2526f8514ef98f88ff13435cf6c
ahmedmili <ahmedmili76@gmail.com>
2022-11-08 20:11:10 +0100

    [New] sql script added

---
+++ BackEnd/script.sql
@@ -0,0 +1,90 @@
+CREATE TABLE employer(
+   Id_employer COUNTER,
+   Role VARCHAR(50),
+   emp_nom VARCHAR(50),
+   emp_prenom VARCHAR(50),
+   emp_cin VARCHAR(50),
+   PRIMARY KEY(Id_employer)
+);
+
+CREATE TABLE Registre(
+   Id_Registre COUNTER,
+   etat_person VARCHAR(50),
+   PRIMARY KEY(Id_Registre)
+);
+
+CREATE TABLE etudiant(
+   Id_etudiant COUNTER,
+   nom_etu VARCHAR(50),
+   prenom_etu VARCHAR(50),
+   cin_etu VARCHAR(50),
+   PRIMARY KEY(Id_etudiant)
+);
+
+CREATE TABLE date_table(
+   Id_date COUNTER,
+   date_time DATETIME,
+   PRIMARY KEY(Id_date)
+);
+
+CREATE TABLE matier(
+   Id_matier COUNTER,
+   mat_name VARCHAR(50),
+   PRIMARY KEY(Id_matier)
+);
+
+CREATE TABLE group_table(
+   Id_group COUNTER,
+   group_name VARCHAR(50),
+   Id_etudiant INT NOT NULL,
+   PRIMARY KEY(Id_group),
+   FOREIGN KEY(Id_etudiant) REFERENCES etudiant(Id_etudiant)
+);
+
+CREATE TABLE filieres(
+   Id_filieres COUNTER,
+   fil_name VARCHAR(50),
+   Id_matier INT NOT NULL,
+   PRIMARY KEY(Id_filieres),
+   FOREIGN KEY(Id_matier) REFERENCES matier(Id_matier)
+);
+
+CREATE TABLE departements(
+   Id_departements COUNTER,
+   dep_name VARCHAR(50),
+   Id_filieres INT NOT NULL,
+   PRIMARY KEY(Id_departements),
+   FOREIGN KEY(Id_filieres) REFERENCES filieres(Id_filieres)
+);
+
+CREATE TABLE enregistrer(
+   Id_employer INT,
+   Id_Registre INT,
+   PRIMARY KEY(Id_employer, Id_Registre),
+   FOREIGN KEY(Id_employer) REFERENCES employer(Id_employer),
+   FOREIGN KEY(Id_Registre) REFERENCES Registre(Id_Registre)
+);
+
+CREATE TABLE concerne(
+   Id_Registre INT,
+   Id_etudiant INT,
+   PRIMARY KEY(Id_Registre, Id_etudiant),
+   FOREIGN KEY(Id_Registre) REFERENCES Registre(Id_Registre),
+   FOREIGN KEY(Id_etudiant) REFERENCES etudiant(Id_etudiant)
+);
+
+CREATE TABLE correspond(
+   Id_Registre INT,
+   Id_date INT,
+   PRIMARY KEY(Id_Registre, Id_date),
+   FOREIGN KEY(Id_Registre) REFERENCES Registre(Id_Registre),
+   FOREIGN KEY(Id_date) REFERENCES date_table(Id_date)
+);
+
+CREATE TABLE Asso_12(
+   Id_Registre INT,
+   Id_matier INT,
+   PRIMARY KEY(Id_Registre, Id_matier),
+   FOREIGN KEY(Id_Registre) REFERENCES Registre(Id_Registre),
+   FOREIGN KEY(Id_matier) REFERENCES matier(Id_matier)
+);
