76b58239cec894b2c6f205fd3ff7470748cc28e0
youssef <youssefbensaid827@gmail.com>
2022-11-08 17:47:12 +0100

    create function ajouter in controllers file

--- BackEnd/controllers/group.js
+++ BackEnd/controllers/group.js
@@ -1,8 +1,14 @@
 import { db } from "../db";
-//ajout
-export const ajouter=(req , res)=>{
-var id = req.body.id;
-var name = req.body.name;
 
-var sql = "INSERT into group (id , name) VALUES (id , name)";
+//ajout
+export const ajouter = (req , res)=>{
+
+  
+      const q = "INSERT INTO group(`id`,`name`) VALUES (?)";
+      const values = [req.body.id, req.body.name];
+  
+      db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json("group has been created.");
+      });
 }
