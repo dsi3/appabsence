31dcc9560207ab98235784f16adbf2274ea0304b
ahmedmili <ahmedmili76@gmail.com>
2022-11-25 19:43:55 +0100

    [Fix] code cleaning

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,30 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path')
-const cors = require("cors");
-const cookieParser = require("cookie-parser")
-// const session = require('express-session')
+const express = require('express')
+const app = express()
+const gestionEtud = require('./Routes/gestion_etu')
 
-var connection = require('./mySql/db_Connection');
-var routes = require('./controller/router');
+const cors = require("cors")
+app.use(express.json())
+app.use(cors())
+app.use("/gestionEtu",gestionEtud); 
 
-var app = express();
-
-app.use('/', express.static(path.join(__dirname, '/')));
-
-app.use(cors({
-  // origin:"http://localhost:3000",
-  methods:["GET","POST","DELETE","PUT"],
-  credentials: true,
-}));
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-connection.init();
-routes.configure(app);
-
-var server = app.listen(8000, function(){
-  console.log('Server listening on port ' + server.address().port);
+app.listen(3004,()=>{
+    console.log("server  runnig in port 3004")
 });
